61c80de19ccb065229b3022a651de11f0c892800
RasyaPrtma <rasyaputraq17@gmail.com>
2024-06-20 14:33:08 +0700

    add date valid

--- src/modules/articles/ListUserArticle.jsx
+++ src/modules/articles/ListUserArticle.jsx
@@ -47,43 +47,56 @@ const ListUserArticle = ({ token, userArticle, getImage, Edit }) => {
     <>
       <main className="bg-[#272727] flex flex-wrap justify-center gap-[3rem] px-[5rem] py-[5rem]">
         {userArticle.length > 0
-          ? userArticle.map((val, index) => (
-              <ul
-                key={val.id}
-                className="w-[500px] h-[auto] bg-[#5e5e5e] rounded-lg flex flex-col flex-wrap justify-center items-center gap-[10px] pb-[3rem]"
-              >
-                <li className="">
-                  <img
-                    className="w-[500px] h-[300px] object-cover rounded-t-lg"
-                    src={imageUrls[index]}
-                    alt={"img"}
-                  />
-                </li>
-                <li className="text-[#0c0e0ce7] uppercase font-semibold text-[1.1em] text-[#f8f8f8] w-[100%] text-center">
-                  {val.title}
-                </li>
-                <li className="text-[#0c0e0ce7] font-medium text-[1.10em] text-[#f8f8f8] text-center">
-                  {val.article}
-                </li>
-                <li className="text-[#0c0e0ce7] font-thin text-[14px] text-[#f8f8f8]">
-                  Uploaded At : {val.uploaded_at}
-                </li>
-                <div className="flex gap-[20px] mt-[10px]">
-                  <button
-                    className="bg-[#27af27] text-[#f8f8f8] w-[80px] py-[5px] rounded-md"
-                    onClick={() => Edit(val.id)}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className="bg-[#e23131] text-[#f8f8f8] w-[80px] py-[5px] rounded-md"
-                    onClick={() => handleDelete(val.id)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </ul>
-            ))
+          ? userArticle.map((val, index) => {
+              const uploadedDate = new Date(val.uploaded_at);
+              const formattedDate = uploadedDate.toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              });
+
+              return (
+                <ul
+                  key={val.id}
+                  className="w-[500px] h-[100%] bg-[#5e5e5e] rounded-lg flex flex-col pb-[2em] items-center relative"
+                >
+                  <li className="">
+                    <img
+                      className="w-[500px] h-[300px] object-cover rounded-t-lg"
+                      src={imageUrls[index]}
+                      alt={"img"}
+                    />
+                  </li>
+                  <div className="flex flex-col items-center px-[2rem] py-[20px]">
+                    <li className="text-[#0c0e0ce7] uppercase font-semibold text-[1.1em] text-[#f8f8f8] w-[100%] text-center">
+                      {val.title}
+                    </li>
+                    <li className="text-[#0c0e0ce7] font-medium text-[1.10em] text-[#f8f8f8] text-center">
+                      {val.article}
+                    </li>
+                  </div>
+
+                  <div className="flex gap-[20px] mt-[10px] mb-[20px]">
+                    <button
+                      className="bg-[#27af27] text-[#f8f8f8] w-[80px] py-[5px] rounded-md"
+                      onClick={() => Edit(val.id)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="bg-[#e23131] text-[#f8f8f8] w-[80px] py-[5px] rounded-md"
+                      onClick={() => handleDelete(val.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+
+                  <li className="text-[#0c0e0ce7] font-thin text-[14px] text-[#f8f8f8] absolute bottom-[15px]">
+                    Uploaded At : {formattedDate}
+                  </li>
+                </ul>
+              );
+            })
           : null}
       </main>
     </>
