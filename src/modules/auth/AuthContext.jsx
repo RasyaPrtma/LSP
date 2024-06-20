486c58cdfc2c44ab1c07bf7e383f4d4580bd897f
RasyaPrtma <rasyaputraq17@gmail.com>
2024-06-20 10:19:09 +0700

    add username for halo

--- src/modules/auth/AuthContext.jsx
+++ src/modules/auth/AuthContext.jsx
@@ -18,11 +18,12 @@ import Cookies from "js-cookie";
  */
 
 const initContext = {
-    doLogin: () => {},
-    doRegist: () => {},
-    doLogout: () => {},
-    IsLogged: true,
-    token: null
+  doLogin: () => {},
+  doRegist: () => {},
+  doLogout: () => {},
+  IsLogged: true,
+  token: null,
+  nameUser: "",
 };
 
 /**
@@ -36,7 +37,7 @@ const AuthContext = createContext(initContext);
  * @returns {AuthContextType} Konteks otentikasi.
  */
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
 
 /**
@@ -46,100 +47,107 @@ export const useAuth = () => {
  * @returns {JSX.Element} Provider konteks otentikasi.
  */
 export const AuthProvider = ({ children }) => {
-    const [IsLogged, setIsLogged] = useState(false);
-    const [token, setToken] = useState(null);
+  const [IsLogged, setIsLogged] = useState(false);
+  const [token, setToken] = useState(null);
+  const [nameUser, setNameUser] = useState("");
 
-    /**
-     * Melakukan proses login pengguna.
-     * @param {string} email - Email pengguna untuk login.
-     * @param {string} password - Kata sandi pengguna untuk login.
-     * @param {Function} clear - Fungsi untuk membersihkan input setelah login berhasil.
-     * @param {Function} close - Fungsi untuk menutup modal setelah login berhasil.
-     * @returns {Promise<boolean>} Promise yang menunjukkan apakah login berhasil atau tidak.
-     */
-    const doLogin = async (email, password, clear, close) => {
-        try {
-            const api = await axios.post(`${HTTP}login`, {
-                email: email,
-                password: password
-            });
+  /**
+   * Melakukan proses login pengguna.
+   * @param {string} email - Email pengguna untuk login.
+   * @param {string} password - Kata sandi pengguna untuk login.
+   * @param {Function} clear - Fungsi untuk membersihkan input setelah login berhasil.
+   * @param {Function} close - Fungsi untuk menutup modal setelah login berhasil.
+   * @returns {Promise<boolean>} Promise yang menunjukkan apakah login berhasil atau tidak.
+   */
+  const doLogin = async (email, password, clear, close) => {
+    try {
+      const api = await axios.post(`${HTTP}login`, {
+        email: email,
+        password: password,
+      });
 
-            if (api.status === 200) {
-                const { data } = api.data;
-                Cookies.set("SESSION_TOKEN", data.token, { expires: 7 });
-                setIsLogged(true);
-                close();
-                alert(api.data.message);
-                clear();
-                return true;
-            } else {
-                const { data } = api;
-                alert(data.message);
-                return false;
-            }
-        } catch (error) {
-            console.error("Error:", error);
-            alert("Login gagal. Silakan coba lagi.");
-            return false;
-        }
-    };
+      if (api.status === 200) {
+        const { data } = api.data;
+        Cookies.set("SESSION_TOKEN", data.token, { expires: 7 });
+        Cookies.set("SESSION_USER", data.username);
+        setNameUser(data.username);
+        setIsLogged(true);
+        close();
+        alert(api.data.message);
+        clear();
+        return true;
+      } else {
+        const { data } = api;
+        alert(data.message);
+        return false;
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Login gagal. Silakan coba lagi.");
+      return false;
+    }
+  };
 
-    /**
-     * Melakukan proses registrasi pengguna.
-     * @param {string} name - Nama lengkap pengguna untuk registrasi.
-     * @param {string} username - Username pengguna untuk registrasi.
-     * @param {string} email - Email pengguna untuk registrasi.
-     * @param {string} password - Kata sandi pengguna untuk registrasi.
-     * @param {Function} clear - Fungsi untuk membersihkan input setelah registrasi berhasil.
-     * @param {Function} close - Fungsi untuk menutup modal setelah registrasi berhasil.
-     */
-    const doRegist = async (name, username, email, password, clear, close) => {
-        try {
-            const api = await axios.post(`${HTTP}register`, {
-                name: name,
-                username: username,
-                email: email,
-                password: password
-            });
+  /**
+   * Melakukan proses registrasi pengguna.
+   * @param {string} name - Nama lengkap pengguna untuk registrasi.
+   * @param {string} username - Username pengguna untuk registrasi.
+   * @param {string} email - Email pengguna untuk registrasi.
+   * @param {string} password - Kata sandi pengguna untuk registrasi.
+   * @param {Function} clear - Fungsi untuk membersihkan input setelah registrasi berhasil.
+   * @param {Function} close - Fungsi untuk menutup modal setelah registrasi berhasil.
+   */
+  const doRegist = async (name, username, email, password, clear, close) => {
+    try {
+      const api = await axios.post(`${HTTP}register`, {
+        name: name,
+        username: username,
+        email: email,
+        password: password,
+      });
 
-            const { message } = api.data;
+      const { message } = api.data;
 
-            if (api.status === 201) {
-                clear();
-                close();
-                alert(message);
-            } else {
-                alert(message);
-            }
-        } catch (error) {
-            console.error("Error:", error);
-            alert("Registrasi gagal. Silakan coba lagi.");
-        }
-    };
+      if (api.status === 201) {
+        clear();
+        close();
+        alert(message);
+      } else {
+        alert(message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Registrasi gagal. Silakan coba lagi.");
+    }
+  };
 
-    /**
-     * Melakukan proses logout pengguna.
-     * @returns {void}
-     */
-    const doLogout = () => {
-        setIsLogged(false);
-        return Cookies.remove("SESSION_TOKEN", { expires: 7 });
-    };
+  /**
+   * Melakukan proses logout pengguna.
+   * @returns {void}
+   */
+  const doLogout = () => {
+    setIsLogged(false);
+    return Cookies.remove("SESSION_TOKEN", { expires: 7 });
+  };
 
-    /**
-     * Efek samping untuk memeriksa token pada Cookies saat komponen dimuat.
-     */
-    useEffect(() => {
-        const token = Cookies.get("SESSION_TOKEN");
-        if (token !== undefined) {
-            setToken(token);
-            setIsLogged(true);
-        }
-    }, []);
+  /**
+   * Efek samping untuk memeriksa token pada Cookies saat komponen dimuat.
+   */
+  useEffect(() => {
+    const token = Cookies.get("SESSION_TOKEN");
+    const username = Cookies.get("SESSION_USER");
+    if (token !== undefined) {
+      setToken(token);
+      setIsLogged(true);
+      setNameUser(username);
+    }
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{ doLogin, doRegist, doLogout, IsLogged, token }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{ doLogin, doRegist, doLogout, IsLogged, token, nameUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- src/modules/layout/NavBar.jsx
+++ src/modules/layout/NavBar.jsx
@@ -10,153 +10,283 @@ import { useAuth } from "../auth/AuthContext";
  * @returns {JSX.Element} Komponen navigasi utama.
  */
 const NavBar = () => {
-    const { doLogin, doRegist, doLogout, IsLogged } = useAuth();
+  const { doLogin, doRegist, doLogout, IsLogged, nameUser } = useAuth();
 
-    const name = useRef();
-    const email = useRef();
-    const username = useRef();
-    const password = useRef();
+  const name = useRef();
+  const email = useRef();
+  const username = useRef();
+  const password = useRef();
 
-    const [loginClick, setLoginClick] = useState(false);
-    const [registClick, setRegistClick] = useState(false);
+  const [loginClick, setLoginClick] = useState(false);
+  const [registClick, setRegistClick] = useState(false);
 
-    /**
-     * Menangani klik untuk menampilkan form login.
-     */
-    const handleLoginClick = () => {
-        setLoginClick(true);
-        setRegistClick(false);
-    }
+  /**
+   * Menangani klik untuk menampilkan form login.
+   */
+  const handleLoginClick = () => {
+    setLoginClick(true);
+    setRegistClick(false);
+  };
 
-    /**
-     * Menangani klik untuk menampilkan form registrasi.
-     */
-    const handleRegisterClick = () => {
-        setRegistClick(true);
-        setLoginClick(false);
-    }
+  /**
+   * Menangani klik untuk menampilkan form registrasi.
+   */
+  const handleRegisterClick = () => {
+    setRegistClick(true);
+    setLoginClick(false);
+  };
 
-    /**
-     * Menutup form login atau registrasi.
-     */
-    const handleClose = () => {
-        setLoginClick(false);
-        setRegistClick(false);
-    }
+  /**
+   * Menutup form login atau registrasi.
+   */
+  const handleClose = () => {
+    setLoginClick(false);
+    setRegistClick(false);
+  };
 
-    /**
-     * Menangani submit form login.
-     * @param {Event} e - Objek event dari form submit.
-     */
-    const handleSubmitLogin = (e) => {
-        e.preventDefault();
-        doLogin(email.current.value, password.current.value, handleClear, handleClose);
-    }
-
-    /**
-     * Menangani submit form registrasi.
-     * @param {Event} e - Objek event dari form submit.
-     */
-    const handleSubmitRegist = (e) => {
-        e.preventDefault();
-        doRegist(name.current.value, username.current.value, email.current.value, password.current.value, handleClear, handleClose);
-    }
-
-    /**
-     * Mengosongkan nilai input pada form login atau registrasi.
-     */
-    const handleClear = () => {
-        if (loginClick) {
-            email.current.value = '';
-            password.current.value = '';
-        } else if (registClick) {
-            email.current.value = '';
-            password.current.value = '';
-            name.current.value = '';
-            username.current.value = '';
-        }
-    }
-
-    /**
-     * Menangani logout pengguna setelah konfirmasi.
-     */
-    const handleLogout = () => {
-        const answer = window.confirm('Yakin');
-        if (answer) {
-            doLogout();
-        }
-    }
-
-    return (
-        <>
-            <header className="w-[100%] py-[1.8em] fixed top-[0] z-[100] left-0 right-0">
-                <nav className="flex justify-between items-center mx-[2.5em]">
-                    <NavLink to={"/"} className="text-[1.5em] font-bold text-[#F8F8F8]">ARTICULATE</NavLink>
-                    {
-                        IsLogged ?
-                            <>
-                                <NavLink to={"/article"} className="font-bold text-[#F8F8F8]">ARTICLE</NavLink>
-                                <div className="flex items-center justify-center gap-[10px]">
-                                    <NavLink className="font-bold text-[#F8F8F8]">HALO USER</NavLink>
-                                    <span className="font-bold text-[#F8F8F8]"> | </span>
-                                    <NavLink onClick={handleLogout} className="font-bold text-[#F8F8F8]">LOG OUT</NavLink>
-                                </div>
-                            </>
-                            :
-                            <div>
-                                <NavLink onClick={handleLoginClick} className="font-bold text-[#F8F8F8]">LOGIN</NavLink>
-                                <span className="font-bold text-[#F8F8F8]"> | </span>
-                                <NavLink onClick={handleRegisterClick} className="font-bold text-[#F8F8F8]">SIGN UP</NavLink>
-                            </div>
-                    }
-                </nav>
-            </header>
-
-            {
-                loginClick ?
-                    <form onSubmit={(e) => handleSubmitLogin(e)} className="fixed top-[5vw] right-[5vw] z-[20] bg-[rgba(255,255,255,0.5)] w-[300px] h-[300px] rounded-md flex justify-center flex-col items-center">
-                        <span className="cursor-pointer absolute top-[20px] left-[25px] text-[14px] text-[rgba(255,255,255,0.8)]" onClick={handleClose}><i className="fa-solid fa-x"></i></span>
-                        <h1 className="text-[1.4em] font-bold text-[rgba(255,255,255,0.8)]">Login</h1>
-                        <div className="flex flex-col mb-[1em]">
-                            <label htmlFor="email" className="text-[14px] text-[rgba(255,255,255,0.8)]">Email</label>
-                            <input autoComplete="off" ref={email} type="email" id="email" className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]" required />
-                        </div>
-                        <div className="flex flex-col mb-[1em]">
-                            <label htmlFor="password" className="text-[14px] text-[rgba(255,255,255,0.8)]">Password</label>
-                            <input autoComplete="off" ref={password} type="password" className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]" id="password" required />
-                        </div>
-                        <button type="submit" className="bg-[rgba(0,0,0,0.2)] text-[rgba(255,255,255,0.8)] text-[14px] px-[15px] py-[6px] rounded-md">Submit</button>
-                    </form>
-                    : null
-            }
-
-            {
-                registClick ?
-                    <form onSubmit={(e) => handleSubmitRegist(e)} className="fixed top-[5vw] right-[5vw] z-[20] bg-[rgba(255,255,255,0.5)] w-[350px] h-[400px] rounded-md flex justify-center flex-col items-center">
-                        <span className="cursor-pointer absolute top-[20px] left-[25px] text-[14px] text-[rgba(255,255,255,0.8)]" onClick={handleClose}><i className="fa-solid fa-x"></i></span>
-                        <h1 className="text-[1.4em] font-bold text-[rgba(255,255,255,0.8)]">Registrasi</h1>
-                        <div className="flex flex-col mb-[1em]">
-                            <label htmlFor="name" className="text-[14px] text-[rgba(255,255,255,0.8)]">Name</label>
-                            <input autoComplete="off" ref={name} type="text" id="name" className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]" required />
-                        </div>
-                        <div className="flex flex-col mb-[1em]">
-                            <label htmlFor="username" className="text-[14px] text-[rgba(255,255,255,0.8)]">Username</label>
-                            <input autoComplete="off" ref={username} type="text" className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]" id="username" required />
-                        </div>
-                        <div className="flex flex-col mb-[1em]">
-                            <label htmlFor="email" className="text-[14px] text-[rgba(255,255,255,0.8)]">Email</label>
-                            <input autoComplete="off" ref={email} type="email" className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]" id="email" required />
-                        </div>
-                        <div className="flex flex-col mb-[1em]">
-                            <label htmlFor="password" className="text-[14px] text-[rgba(255,255,255,0.8)]">Password</label>
-                            <input autoComplete="off" ref={password} type="password" className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]" id="password" required />
-                        </div>
-                        <button type="submit" className="bg-[rgba(0,0,0,0.2)] text-[rgba(255,255,255,0.8)] text-[14px] px-[15px] py-[6px] rounded-md">Submit</button>
-                    </form>
-                    : null
-            }
-        </>
+  /**
+   * Menangani submit form login.
+   * @param {Event} e - Objek event dari form submit.
+   */
+  const handleSubmitLogin = (e) => {
+    e.preventDefault();
+    doLogin(
+      email.current.value,
+      password.current.value,
+      handleClear,
+      handleClose
     );
-}
+  };
 
-export default NavBar
+  /**
+   * Menangani submit form registrasi.
+   * @param {Event} e - Objek event dari form submit.
+   */
+  const handleSubmitRegist = (e) => {
+    e.preventDefault();
+    doRegist(
+      name.current.value,
+      username.current.value,
+      email.current.value,
+      password.current.value,
+      handleClear,
+      handleClose
+    );
+  };
+
+  /**
+   * Mengosongkan nilai input pada form login atau registrasi.
+   */
+  const handleClear = () => {
+    if (loginClick) {
+      email.current.value = "";
+      password.current.value = "";
+    } else if (registClick) {
+      email.current.value = "";
+      password.current.value = "";
+      name.current.value = "";
+      username.current.value = "";
+    }
+  };
+
+  /**
+   * Menangani logout pengguna setelah konfirmasi.
+   */
+  const handleLogout = () => {
+    const answer = window.confirm("Yakin");
+    if (answer) {
+      doLogout();
+    }
+  };
+
+  return (
+    <>
+      <header className="w-[100%] py-[1.8em] fixed top-[0] z-[100] left-0 right-0">
+        <nav className="flex justify-between items-center mx-[2.5em]">
+          <NavLink to={"/"} className="text-[1.5em] font-bold text-[#F8F8F8]">
+            ARTICULATE
+          </NavLink>
+          {IsLogged ? (
+            <>
+              <NavLink to={"/article"} className="font-bold text-[#F8F8F8]">
+                ARTICLE
+              </NavLink>
+              <div className="flex items-center justify-center gap-[10px]">
+                <NavLink className="font-bold text-[#F8F8F8]">
+                  HALO {nameUser}
+                </NavLink>
+                <span className="font-bold text-[#F8F8F8]"> | </span>
+                <NavLink
+                  onClick={handleLogout}
+                  className="font-bold text-[#F8F8F8]"
+                >
+                  LOG OUT
+                </NavLink>
+              </div>
+            </>
+          ) : (
+            <div>
+              <NavLink
+                onClick={handleLoginClick}
+                className="font-bold text-[#F8F8F8]"
+              >
+                LOGIN
+              </NavLink>
+              <span className="font-bold text-[#F8F8F8]"> | </span>
+              <NavLink
+                onClick={handleRegisterClick}
+                className="font-bold text-[#F8F8F8]"
+              >
+                SIGN UP
+              </NavLink>
+            </div>
+          )}
+        </nav>
+      </header>
+
+      {loginClick ? (
+        <form
+          onSubmit={(e) => handleSubmitLogin(e)}
+          className="fixed top-[5vw] right-[5vw] z-[20] bg-[rgba(255,255,255,0.5)] w-[300px] h-[300px] rounded-md flex justify-center flex-col items-center"
+        >
+          <span
+            className="cursor-pointer absolute top-[20px] left-[25px] text-[14px] text-[rgba(255,255,255,0.8)]"
+            onClick={handleClose}
+          >
+            <i className="fa-solid fa-x"></i>
+          </span>
+          <h1 className="text-[1.4em] font-bold text-[rgba(255,255,255,0.8)]">
+            Login
+          </h1>
+          <div className="flex flex-col mb-[1em]">
+            <label
+              htmlFor="email"
+              className="text-[14px] text-[rgba(255,255,255,0.8)]"
+            >
+              Email
+            </label>
+            <input
+              autoComplete="off"
+              ref={email}
+              type="email"
+              id="email"
+              className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-[1em]">
+            <label
+              htmlFor="password"
+              className="text-[14px] text-[rgba(255,255,255,0.8)]"
+            >
+              Password
+            </label>
+            <input
+              autoComplete="off"
+              ref={password}
+              type="password"
+              className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]"
+              id="password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-[rgba(0,0,0,0.2)] text-[rgba(255,255,255,0.8)] text-[14px] px-[15px] py-[6px] rounded-md"
+          >
+            Submit
+          </button>
+        </form>
+      ) : null}
+
+      {registClick ? (
+        <form
+          onSubmit={(e) => handleSubmitRegist(e)}
+          className="fixed top-[5vw] right-[5vw] z-[20] bg-[rgba(255,255,255,0.5)] w-[350px] h-[400px] rounded-md flex justify-center flex-col items-center"
+        >
+          <span
+            className="cursor-pointer absolute top-[20px] left-[25px] text-[14px] text-[rgba(255,255,255,0.8)]"
+            onClick={handleClose}
+          >
+            <i className="fa-solid fa-x"></i>
+          </span>
+          <h1 className="text-[1.4em] font-bold text-[rgba(255,255,255,0.8)]">
+            Registrasi
+          </h1>
+          <div className="flex flex-col mb-[1em]">
+            <label
+              htmlFor="name"
+              className="text-[14px] text-[rgba(255,255,255,0.8)]"
+            >
+              Name
+            </label>
+            <input
+              autoComplete="off"
+              ref={name}
+              type="text"
+              id="name"
+              className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-[1em]">
+            <label
+              htmlFor="username"
+              className="text-[14px] text-[rgba(255,255,255,0.8)]"
+            >
+              Username
+            </label>
+            <input
+              autoComplete="off"
+              ref={username}
+              type="text"
+              className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]"
+              id="username"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-[1em]">
+            <label
+              htmlFor="email"
+              className="text-[14px] text-[rgba(255,255,255,0.8)]"
+            >
+              Email
+            </label>
+            <input
+              autoComplete="off"
+              ref={email}
+              type="email"
+              className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]"
+              id="email"
+              required
+            />
+          </div>
+          <div className="flex flex-col mb-[1em]">
+            <label
+              htmlFor="password"
+              className="text-[14px] text-[rgba(255,255,255,0.8)]"
+            >
+              Password
+            </label>
+            <input
+              autoComplete="off"
+              ref={password}
+              type="password"
+              className="w-[250px] rounded-sm border-[none] outline-none pl-2 text-[14px] py-[5px] text-[rgba(0,0,0,0.5)]"
+              id="password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-[rgba(0,0,0,0.2)] text-[rgba(255,255,255,0.8)] text-[14px] px-[15px] py-[6px] rounded-md"
+          >
+            Submit
+          </button>
+        </form>
+      ) : null}
+    </>
+  );
+};
+
+export default NavBar;
